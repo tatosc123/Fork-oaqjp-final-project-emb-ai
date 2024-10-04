19ba2aacbea13c0571d7fe2bf709ec7ea7116241
tatosc123 <tatosc@msn.com>
2024-10-04 00:18:35 -0400

    last commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,6 @@
 import requests # Import the requests library
 import json
-text_to_analyze = 'I love you'
+
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
