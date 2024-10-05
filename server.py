a1663be227b23d283b586cf8edcefc92e21e8b72
tatosc123 <tatosc@msn.com>
2024-10-04 22:48:17 -0400

    last commit

--- server.py
+++ server.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
-
 @app.route("/emotionDetector")
 def sent_analizer():
     # Retrive text to analize from the request arguments
-    text_to_analyze = request.args.get('textToAnalize')
+    text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion detector function
     response = emotion_detector(text_to_analyze)
     
     # Extract the label and the score for the response
-
-  
+    
+    return response
+    
 
 @app.route("/")
 def render_index_page():
